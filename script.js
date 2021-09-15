376a00b0473109429b3627d603492d347787dde4
Kevin Eschbach <eshy@gmx.at>
2021-09-15 23:25:47 +0200

    Add animation

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ $(document).ready(function(){
         }
     })
 })
+
+document.addEventListener('DOMContentLoaded', function(event){
+    var textData = ["Student", "Social Democrat", "Software Developer"];
+
+    function typewriter(text, i, fncCallback){
+        if(i < text.length){
+            document.querySelector("#what-am-i").innerHTML = text.substring(0, i+1);
+            setTimeout(function() {
+                typewriter(text, i+1, fncCallback)
+            }, 100);
+        }
+        else if (typeof fncCallback == 'function'){
+            setTimeout(fncCallback, 700);
+        }
+    }
+
+    function startTextAnimation(i) {
+        if(i < textData.length){
+            typewriter(textData[i], 0, function(){
+                startTextAnimation(i+1);
+            });
+        } else {
+            setTimeout(function(){
+                startTextAnimation(0);
+            }, 20000);
+        }
+    }
+
+    startTextAnimation(0)
+});
